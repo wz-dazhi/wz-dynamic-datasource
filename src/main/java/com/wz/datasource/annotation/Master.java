8b4be2357fee4fac56730428255bfa4b394fd533
wz-dazhi <742820195@qq.com>
2022-12-26 14:54:32 +0800

    first commit

---
+++ src/main/java/com/wz/datasource/annotation/Master.java
@@ -0,0 +1,17 @@
+package com.wz.datasource.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @package: com.wz.datasource.annotation
+ * @className: Master
+ * @description:
+ * @author: Zhi
+ * @date: 2020-04-13 16:29
+ * @version: 1.0
+ */
+@Target(ElementType.METHOD)
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Master {
+}
